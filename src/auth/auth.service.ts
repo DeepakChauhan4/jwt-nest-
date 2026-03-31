1569260682cbfa10b8f0a128825c98ed7dcc0cc7
Deepak Chauhan <deepak41125@gmail.com>
2026-03-31 14:57:36 +0530

    Initial commit: NestJS JWT auth app with users module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+type AuthInput = { username: string, password: string };
+type SignInData = { accessToken: string, userId: number, username: string };
+type AuthResult = { accessToken: string, userId: number, username: string };
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService) { }
+
+    async authenticate(input: AuthInput): Promise<AuthResult> {
+        const user = await this.validateUser(input);
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        return {
+            accessToken: user.accessToken,
+            userId: user.userId,
+            username: user.username,
+        };
+    }
+    async validateUser(input: AuthInput): Promise<SignInData | null> {
+        const user = await this.usersService.findUserByName(input.username);
+        if (user && user.password === input.password) {
+            return {
+                accessToken: 'fake-access',
+                userId: user.userId,
+                username: user.username,
+            };
+        }
+        return null;
+    }
+}
